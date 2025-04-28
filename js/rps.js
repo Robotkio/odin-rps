ea07b7c320722365cf174fbc2d6df8000b9c52cf
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-04-27 21:46:28 -0700

    add playRound() functionality add playGame() functionality

--- js/rps.js
+++ js/rps.js
@@ -2,6 +2,9 @@ const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     switch (Math.floor(Math.random() * 3)) {
         case 0: return rock;
@@ -20,12 +23,69 @@ function getHumanChoice() {
     return "ERROR: getHumanChoice()";
 }
 
-function playRound() {
+function playRound(humanChoice, computerChoice) {
+    // determine winner
+    // winner -1: human
+    // winner  0: tie
+    // winner  1: computer
+    let winner = 0;
+    switch ([humanChoice, computerChoice]) {
+        case [rock, rock]:
+            winner = 0;
+            break;
+        case [rock, paper]:
+            winner = 1;
+            break;
+        case [rock, scissors]:
+            winner = -1;
+            break;
+        case [paper, paper]:
+            winner = 0;
+            break;
+        case [paper, rock]:
+            winner = -1;
+            break;
+        case [paper, scissors]:
+            winner = 1;
+            break;
+        case [scissors, scissors]:
+            winner = 0;
+            break;
+        case [scissors, rock]:
+            winner = 1;
+            break;
+        case [scissors, paper]:
+            winner = -1;
+            break;
+        default:
+            winner = 0;
+    }
 
+    // print winning line and update score
+    switch (winner) {
+        case -1:
+            console.log(`Human wins! ${humanChoice} beats ${computerChoice}!`);
+            humanScore++;
+            break;
+        case 0:
+            console.log(`Tie! ${humanChoice} and ${computerChoice} are the same!`);
+            break;
+        case 1:
+            console.log(`Computer wins! ${computerChoice} beats ${humanChoice}!`);
+            computerScore++;
+            break;
+        default:
+            console.log("ERROR: playRound()");
+    }
 }
 
 function playGame() {
+    humanScore = 0;
+    computerScore = 0;
 
+    for (let i = 0; i < 5; i++) {
+        playRound(humanChoice(), computerChoice());
+    }
 }
 
 module.exports = {
