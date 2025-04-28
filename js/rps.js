79d7b8ece0e6cd4bcfb921c5cd46230f85a50538
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-04-27 21:16:39 -0700

    add getComputerChoice functionality

--- js/rps.js
+++ js/rps.js
@@ -1,5 +1,10 @@
 function getComputerChoice() {
-
+    switch (Math.floor(Math.random() * 3)) {
+        case 0: return "rock";
+        case 1: return "paper";
+        case 2: return "scissors";
+        default: return "ERROR";
+    }
 }
 
 function getHumanChoice() {
