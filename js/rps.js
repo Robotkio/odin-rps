db85a65f94210d8e10b6fd75c4821568fbff888d
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-04-27 21:19:11 -0700

    add rock, paper, scissors constants modify getComputerChoice to use these

--- js/rps.js
+++ js/rps.js
@@ -1,9 +1,13 @@
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+
 function getComputerChoice() {
     switch (Math.floor(Math.random() * 3)) {
-        case 0: return "rock";
-        case 1: return "paper";
-        case 2: return "scissors";
-        default: return "ERROR";
+        case 0: return rock;
+        case 1: return paper;
+        case 2: return scissors;
+        default: return "ERROR: getComputerChoice()";
     }
 }
 
