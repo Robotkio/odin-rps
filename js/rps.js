0c0445bc0fd360789ad2e0183cde8a8f02d0c7e4
Robotkio <61295708+Robotkio@users.noreply.github.com>
2025-04-29 21:54:59 -0700

    add win functionality add enable/disable button functions

--- js/rps.js
+++ js/rps.js
@@ -5,18 +5,21 @@ const scissors = "scissors";
 let humanScore = 0;
 let computerScore = 0;
 let roundNumber = 0;
+const scoreLimit = 5;
 
 let statusPannel = document.getElementById("rps-status");
 let resultPannel = document.getElementById("rps-results");
 
-function setupGame() {
-    let btnRock = document.getElementById("btn-rock");
-    let btnPaper = document.getElementById("btn-paper");
-    let btnScissors = document.getElementById("btn-scissors");
+let btnRock = document.getElementById("btn-rock");
+let btnPaper = document.getElementById("btn-paper");
+let btnScissors = document.getElementById("btn-scissors");
+let btnReset = document.getElementById("btn-reset");
 
+function setupGame() {
     btnRock.addEventListener("click", () => {playRound(rock, getComputerChoice())});
     btnPaper.addEventListener("click", () => {playRound(paper, getComputerChoice())});
     btnScissors.addEventListener("click", () => {playRound(scissors, getComputerChoice())});
+    btnReset.addEventListener("click", () => {resetGame()});
 
     updateStatusPannel();
 }
@@ -86,6 +89,49 @@ function playRound(humanChoice, computerChoice) {
     }
     resultPannel.appendChild(outputMessage);
     updateStatusPannel();
+
+    // check if game is over by hitting score limit and display message
+    if (humanScore >= scoreLimit || computerScore >= scoreLimit) {
+        outputMessage = document.createElement("div");
+        outputMessage.classList.add("output-msg");
+        if (humanScore > computerScore) {
+            outputMessage.innerText = `Human was first to ${scoreLimit} wins! Computer got ${computerScore}`;
+            outputMessage.classList.add("game-win");
+        } else if (computerScore > humanScore) {
+            outputMessage.innerText = `Computer was first to ${scoreLimit} wins... Human got ${humanScore}`;
+            outputMessage.classList.add("game-lose");
+        } else {
+            outputMessage.innerText = `It was a tie? ${computerScore} to ${humanScore}`;
+            outputMessage.classList.add("game-ltie");
+        }
+        resultPannel.appendChild(outputMessage);
+        disableButtons();
+    }
+}
+
+function enableButtons() {
+    btnRock.disabled = false;
+    btnPaper.disabled = false;
+    btnScissors.disabled = false;
+}
+
+function disableButtons() {
+    btnRock.disabled = true;
+    btnPaper.disabled = true;
+    btnScissors.disabled = true;
+}
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    roundNumber = 0;
+    updateStatusPannel();
+    clearResultsPannel();
+    enableButtons();
+}
+
+function clearResultsPannel() {
+    resultPannel.innerHTML = "";
 }
 
 function updateStatusPannel() {
